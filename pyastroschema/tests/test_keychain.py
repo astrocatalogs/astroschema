e0f28012184d72de1e49e9a8e8dd6d19d86d58d5
lzkelley <lkelley@cfa.harvard.edu>
2018-07-02 10:52:16 -0400

    Update keychain tests for new 'Key' class and structure

--- pyastroschema/tests/test_keychain.py
+++ pyastroschema/tests/test_keychain.py
@@ -10,7 +10,7 @@ Can be run with:
 from nose.tools import assert_true, assert_raises  # , assert_false, assert_equal,
 
 # import pyastroschema as pas
-from pyastroschema.keys import Keychain
+from pyastroschema.keys import Keychain, Key
 
 
 SIMPLEST_SCHEMA = dict(
@@ -31,27 +31,21 @@ SIMPLEST_SCHEMA = dict(
 
 def test_basics():
     keys = Keychain(SIMPLEST_SCHEMA)
-    good_values = ["alias", "name"]
-    good_keys = [gv.upper() for gv in good_values]
-
-    print("Checking values")
-    print("keys.values = ", keys.values())
-    assert_true(len(keys.values()) == 2)
-    assert_true(all([gv in keys.values() for gv in good_values]))
-    #   test the `__contains__` overridden method
-    assert_true(all([gv in keys for gv in good_values]))
+    good_keys = SIMPLEST_SCHEMA['properties'].keys()
 
     print("Checking keys")
-    assert_true(len(keys.keys()) == 2)
+    assert_true(len(keys.keys()) == len(good_keys))
     assert_true(all([gk in keys.keys() for gk in good_keys]))
 
     print("Checking attribute access")
-    assert_true(keys.ALIAS == "alias")
-    assert_true(keys.NAME == "name")
-    with assert_raises(AttributeError):
-        keys.alias
-    with assert_raises(AttributeError):
-        keys.name
+    for kk, vv in SIMPLEST_SCHEMA['properties'].items():
+        # Get each key
+        temp = getattr(keys, kk)
+        # The key should be a string (subclass) equal to its name
+        assert_true(temp == kk)
+        for k2, v2 in vv.items():
+            # The attributes of the key should match the dictionary values
+            assert_true(getattr(temp, k2) == v2)
 
     return
 
@@ -60,123 +54,76 @@ def test_mutable():
     print("Testing mutable=True")
     for extend in [True, False]:
         keys = Keychain(SIMPLEST_SCHEMA, mutable=True, extendable=extend)
-        assert_true(keys.ALIAS == "alias")
+        assert_true(keys.alias == "alias")
 
-        keys.ALIAS = "alias2"
-        assert_true(keys.ALIAS == "alias2")
+        # Added general attribute should not count as a `Key`
+        keys.alias = "alias2"
+        assert_true(keys.alias == "alias2")
         assert_true(len(keys.keys()) == 2)
-        assert_true(len(keys.values()) == 2)
 
     print("Testing mutable=False")
     for extend in [True, False]:
         keys = Keychain(SIMPLEST_SCHEMA, mutable=False, extendable=extend)
-        assert_true(keys.ALIAS == "alias")
+        assert_true(keys.alias == "alias")
 
         with assert_raises(RuntimeError):
-            keys.ALIAS = "alias2"
+            keys.alias = "alias2"
 
-        assert_true(keys.ALIAS == "alias")
+        assert_true(keys.alias == "alias")
         assert_true(len(keys.keys()) == 2)
-        assert_true(len(keys.values()) == 2)
 
     return
 
 
 def test_extendable():
-    print("Test extendable=True")
+
+    print("Testing adding random attribute")
+
+    print("\tTest extendable=True")
     for mutab in [True, False]:
         keys = Keychain(SIMPLEST_SCHEMA, mutable=mutab, extendable=True)
+        good_keys = SIMPLEST_SCHEMA['properties'].keys()
 
-        # should be able to add an arbitrary key
-        keys.CHECK_BIG = "check-big"
-        keys.check_small = "check-small"
-        good_keys = ["CHECK_BIG", "check_small"]
-        good_values = ["check-big", "check-small"]
-
-        print("Checking values")
-        assert_true(len(keys.values()) == 4)
-        assert_true(all([gv in keys.values() for gv in good_values]))
-
-        print("Checking keys")
-        assert_true(len(keys.keys()) == 4)
+        # should be able to add an arbitrary attribute
+        keys.test = "test"
+        print("\tChecking keys")
+        # Should not be counted as a `Key` proper
+        assert_true(len(keys.keys()) == 2)
         assert_true(all([gk in keys.keys() for gk in good_keys]))
 
-    print("Test extendable=False")
+    print("\tTest extendable=False")
     for mutab in [True, False]:
         keys = Keychain(SIMPLEST_SCHEMA, mutable=mutab, extendable=False)
 
         # should be able to add an arbitrary key
         with assert_raises(RuntimeError):
-            keys.CHECK_BIG = "check-big"
-            keys.check_small = "check-small"
+            keys.test = "test"
 
-        assert_true(len(keys.values()) == 2)
         assert_true(len(keys.keys()) == 2)
 
-    return
+    # -------------------
+    print("Testing adding new `Key`")
 
-
-def test_valid_key_val():
-    print("test_valid_key_val()")
-    simplest = dict(
-        properties=dict(
-            alias=dict(type="string", distinguishing=False, num=1)
-        )
-    )
-
-    print("\tChecking baseline")
-    keys = Keychain(simplest)
-    assert_true(len(keys.keys()) == 1)
-
-    print("\tChecking skip underscores")
+    print("\tTest extendable=True")
     for mutab in [True, False]:
-        for extend in [True, False]:
+        keys = Keychain(SIMPLEST_SCHEMA, mutable=mutab, extendable=True)
+        good_keys = SIMPLEST_SCHEMA['properties'].keys()
 
-            for bad_key in ["_test", "__test"]:
-                badest = dict(
-                    properties=dict(
-                        alias=dict(type="string", distinguishing=False, num=1)
-                    )
-                )
-                badest['properties'][bad_key] = dict(
-                    some=bad_key, type="int", distinguishing=False)
-                keys = Keychain(badest, mutable=mutab, extendable=extend)
-                assert_true(len(keys.keys()) == 1)
-                assert_true(len(keys.values()) == 1)
-                assert_true(bad_key not in keys.values())
-                assert_true(bad_key not in keys.keys())
-                assert_true(bad_key.upper() not in keys.keys())
-                # But it should still have the attribute
-                assert_true(hasattr(keys, bad_key.upper()))
-                check_val = getattr(keys, bad_key.upper())
-                assert_true(check_val == bad_key)
+        keys.test = Key("test", type='string', distinguishing=False)
+        good_keys = list(good_keys) + ['test']
+        print("\tChecking keys")
+        # Should not be counted as a `Key` proper
+        assert_true(len(keys.keys()) == len(good_keys))
+        assert_true(all([gk in keys.keys() for gk in good_keys]))
 
-    print("\tChecking skip callables")
+    print("\tTest extendable=False")
     for mutab in [True, False]:
+        keys = Keychain(SIMPLEST_SCHEMA, mutable=mutab, extendable=False)
 
-        good = dict(
-            properties=dict(
-                alias=dict(type="string", distinguishing=False, num=1)
-            )
-        )
-        good_key = "new"
-        good['properties'][good_key] = dict(type='string', distinguishing=False, num=123)
-        keys = Keychain(good, mutable=mutab, extendable=True)
+        # should be able to add an arbitrary key
+        with assert_raises(RuntimeError):
+            keys.test = Key("test", type='string', distinguishing=False)
+
         assert_true(len(keys.keys()) == 2)
-        assert_true(len(keys.values()) == 2)
-        assert_true(good_key.upper() in keys.keys())
-        assert_true(good_key in keys.values())
-
-        bad_key = "new"
-        bad_val = lambda xx: xx*2  # noqa
-        setattr(keys, bad_key, bad_val)
-        assert_true(len(keys.keys()) == 2)
-        assert_true(len(keys.values()) == 2)
-        assert_true(bad_key not in keys.keys())
-        assert_true(bad_val not in keys.values())
-
-        # But it still should have been added to the attributes
-        assert_true(hasattr(keys, bad_key))
-        assert_true(getattr(keys, bad_key) == bad_val)
 
     return
