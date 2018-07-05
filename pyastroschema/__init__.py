65092e7160071478413383f5549a7c7fb6ce3c42
lzkelley <lkelley@cfa.harvard.edu>
2018-07-04 20:31:22 -0400

    Added meta-schema subdirectory and new astro-schema meta-schema file

--- pyastroschema/__init__.py
+++ pyastroschema/__init__.py
@@ -14,6 +14,8 @@ _FNAME_VERSION = "VERSION"
 _INDEX_JSON_FILENAME = "astroschema_index.json"
 _DIR_SCHEMA = "schema"
 _DIR_TESTS = "tests"
+_DIR_META_SCHEMA = "meta-schema"
+_META_SCHEMA_FILENAME = "astro-schema_draft-0.json"
 
 INDEX_DESCRIPTION = "Index and summary of schema included in `astroschema`."
 
@@ -26,10 +28,12 @@ class PATHS:
     ASTROSCHEMA = os.path.realpath(os.path.join(PYASTROSCHEMA, os.path.pardir))
 
     SCHEMA_DIR = os.path.join(ASTROSCHEMA, _DIR_SCHEMA, "")
+    META_SCHEMA_DIR = os.path.join(SCHEMA_DIR, _DIR_META_SCHEMA, "")
     TESTS_DIR = os.path.join(ASTROSCHEMA, _DIR_TESTS, "")
     ASTROSCHEMA_VERSION_FILE = os.path.join(ASTROSCHEMA, _FNAME_VERSION)
     INDEX_JSON_FILE = os.path.join(ASTROSCHEMA, _INDEX_JSON_FILENAME)
 
+    META_SCHEMA_FILE = os.path.join(META_SCHEMA_DIR, _META_SCHEMA_FILENAME)
 
 # print("Path: pyastroschema: '{}'".format(PATHS.PYASTROSCHEMA))
 # print("Path:   astroschema: '{}'".format(PATHS.ASTROSCHEMA))
