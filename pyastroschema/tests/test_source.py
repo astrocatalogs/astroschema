772766f6f328dcdaa6f646b741485c00bce300a0
lzkelley <lkelley@cfa.harvard.edu>
2018-07-16 14:05:39 -0700

    Update comparisons for new 'is_duplicate'

--- pyastroschema/tests/test_source.py
+++ pyastroschema/tests/test_source.py
@@ -18,21 +18,26 @@ from pyastroschema.struct import Source
 
 SIMPLEST_SOURCE = dict(
     alias=0,
-    name="Paper et al 2017"
+    arxivid="1605.01054"
 )
 
 
 def test_basics():
     # source_schema = pas.utils.load_schema('source')
 
-    # Should work with alias and name
+    # Should work with alias and arxivid
     source = Source(**SIMPLEST_SOURCE)
 
     keys = source._keychain
     assert_true(source['alias'] == SIMPLEST_SOURCE['alias'])
-    assert_true(source['name'] == SIMPLEST_SOURCE['name'])
+    assert_true(source['arxivid'] == SIMPLEST_SOURCE['arxivid'])
+    print('alias' in source)
+    print(keys.ALIAS, keys.ALIAS == 'alias')
+    print(hash('alias'), hash(keys.ALIAS))
+    print(keys.ALIAS in source)
+
     assert_true(source[keys.ALIAS] == SIMPLEST_SOURCE['alias'])
-    assert_true(source[keys.NAME] == SIMPLEST_SOURCE['name'])
+    assert_true(source[keys.ARXIVID] == SIMPLEST_SOURCE['arxivid'])
 
     return
 
@@ -48,7 +53,7 @@ def test_validation():
 
     # Should fail with negative alias
     with assert_raises(jsonschema.exceptions.ValidationError):
-        source = Source(name='Test', alias=-1)
+        source = Source(arxivid='Test', alias=-1)
 
     # Should fail with negative alias
     with assert_raises(jsonschema.exceptions.ValidationError):
@@ -83,27 +88,43 @@ def test_duplicate_comparison():
     print("test_source.py:test_duplicate_comparison()")
 
     # Identical `Source`s should show as duplicates
+    '''
     s1 = Source(validate=False)
     s2 = Source(validate=False)
+    print("\nCompare True:")
+    print("s1 = '{}'".format(repr(s1)))
+    print("s2 = '{}'".format(repr(s2)))
     assert_false(s1 is s2)
     assert_true(s1.is_duplicate_of(s2))
     assert_true(s2.is_duplicate_of(s1))
+    '''
 
     s1 = Source(**SIMPLEST_SOURCE)
     s2 = Source(**SIMPLEST_SOURCE)
+    print("\nCompare True:")
+    print("s1 = '{}'".format(repr(s1)))
+    print("s2 = '{}'".format(repr(s2)))
+    print('arxivid' in s1, s1['arxivid'])
+    print('arxivid' in s2, s2['arxivid'])
     assert_false(s1 is s2)
-    assert_true(s1.is_duplicate_of(s2))
-    assert_true(s2.is_duplicate_of(s1))
+    assert_true(s1.is_duplicate_of(s2, verbose=True))
+    assert_true(s2.is_duplicate_of(s1, verbose=True))
 
+    print("\nCompare False:")
     s1 = Source(**SIMPLEST_SOURCE)
     s2 = Source(validate=False)
     # Initialized with different values, duplicate should be false
+    print("s1 = '{}'".format(repr(s1)))
+    print("s2 = '{}'".format(repr(s2)))
     assert_false(s1.is_duplicate_of(s2))
     assert_false(s2.is_duplicate_of(s1))
     # Change values to match
     for kk, vv in SIMPLEST_SOURCE.items():
         s2[kk] = vv
     # Matching values should be duplicates
+    print("\nCompare True:")
+    print("s1 = '{}'".format(repr(s1)))
+    print("s2 = '{}'".format(repr(s2)))
     assert_true(s1.is_duplicate_of(s2))
     assert_true(s2.is_duplicate_of(s1))
 
@@ -112,8 +133,8 @@ def test_duplicate_comparison():
     assert_true(s1.is_duplicate_of(s2))
     assert_true(s2.is_duplicate_of(s1))
 
-    # 'name' is a distinguishing parameter, should affect duplicate test
-    s2['name'] = "different"
+    # 'arxivid' is a distinguishing parameter, should affect duplicate test
+    s2['arxivid'] = "1605.00000"
     assert_false(s1.is_duplicate_of(s2))
     assert_false(s2.is_duplicate_of(s1))
 
