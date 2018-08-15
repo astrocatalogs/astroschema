9a446bb90b70348ac42211ea43bd4c3b267a7511
lzkelley <lkelley@cfa.harvard.edu>
2018-08-15 13:55:15 -0400

    DOCS brief description in setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     version=version,
     author="Luke Zoltan Kelley",
     author_email="lzkelley@gmail.com",
-    # description="",
+    description="Standardized specifications for astronomy and astrophysics data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/astrocatalogs/astroschema",
